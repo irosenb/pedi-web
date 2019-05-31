7c91f146645d661457dcbf20f671411bfa4e5214
Isaac Rosenberg <irosenb7@gmail.com>
2019-05-31 18:01:35 -0400

    Terms of service is up

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var path = require('path');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pedi' });
 });
+
+router.get('/terms', function (req, res, next) {
+  var options = {
+    root: path.join(__dirname, '../public'),
+    dotfiles: 'deny',
+    headers: {
+      'x-timestamp': Date.now(),
+      'x-sent': true
+    }
+  }
+  res.sendFile('/attachments/terms.pdf', options, function (error) {
+    if (error) {
+      console.log(error)
+      next(error);
+    } else {
+
+    }
+  });
+})
 
 module.exports = router;
